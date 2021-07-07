caec4acd941012e417bad8833cbd7e6973a03d68
AnaNicoletti <ananicoletti135@gmail.com>
2021-07-07 20:47:25 +0000

    Adicionei as url

--- modelos/noticia.js
+++ modelos/noticia.js
@@ -1,9 +1,10 @@
 class Noticia{
-  constructor (title, publishedAt, description, author){
-    this._title = title
-    this._publishedAt = publishedAt
-    this._description = description
-    this._author = author
+  constructor (title, publishedAt, description, author, url){
+    this._title = title;
+    this._publishedAt = publishedAt;
+    this._description = description;
+    this._author = author;
+    this._url = url;
   }
 
   get mostrar(){
@@ -17,10 +18,10 @@ class Noticia{
   }
 
   mostrarNoticia(){
-    if (this._title != "" && this._publishedAt != "" && this._description != "" && this._author != "" && this._author != ""){
+    if (this._title != "" && this._publishedAt != "" && this._description != "" && this._author != "" && this._author != "" && this._url != ""){
     
       return`
-      <h3>${this._title}</h3>
+      <h3><a href="${this._url}">${this._title}</a></h3>
       <div>${this._publishedAt}</div>
       <div>${this._description}</div><br>
       <div>${this._author}</div>
